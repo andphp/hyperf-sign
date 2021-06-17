735481f4516766996fe5822022be98d29fa6c8a5
BabySeeME「大雄」 <417170808@qq.com>
2021-06-17 16:57:15 +0800

    first commit

--- src/Annotation/Sign.php
+++ src/Annotation/Sign.php
@@ -37,30 +37,31 @@ class Sign extends AbstractAnnotation
      * @param  [type] $data   [description]
      * @return [type]         [description]
      */
-    public function verifySign(array $data, array $signConfig)
+    public function verifySign(array $param, array $signConfig)
     {
         $data = [
             'code' => 401,
             'msg'  => 'Signature verification failed'
         ];
+
         // 验证参数中是否有签名
-        if (!isset($data['sign']) || !$data['sign']) {
+        if (!isset($param['sign']) || !$param['sign']) {
             $data['msg'] = '发送的数据签名不存在';
             return $data;
         }
-        if (!isset($data['timestamp']) || !$data['timestamp']) {
+        if (!isset($param['timestamp']) || !$param['timestamp']) {
             $data['msg'] = '发送的数据参数不合法';
             return $data;
         }
         // 验证请求， 10分钟失效
-        if (time() - $data['timestamp'] > $signConfig['expires_in']) {
+        if (time() - $param['timestamp'] > $signConfig['expires_in']) {
             $data['msg'] = '验证失效， 请重新发送请求';
             return $data;
         }
-        $sign = $data['sign'];
-        unset($data['sign']);
-        ksort($data);
-        $params = http_build_query($data);
+        $sign = $param['sign'];
+        unset($param['sign']);
+        ksort($param);
+        $params = http_build_query($param);
         // $secret是通过key在api的数据库中查询得到
         $sign2 = md5($params . $signConfig['secret']);
         if ($sign == $sign2) {
